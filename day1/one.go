5274251cf5f84b532b403f1038d88f2a40d518f5
chenjj6843 <chenjj6843@yeastar.com>
2023-09-06 20:49:37 +0800

    合并两个有序数组

---
+++ day1/one.go
@@ -0,0 +1,24 @@
+package day1
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	res := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			res = append(res, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			res = append(res, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			res = append(res, nums1[p1])
+			p1++
+		} else {
+			res = append(res, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, res)
+}
