d385577be7302104b7b5206dfc393f2c1a51bb88
chenjj6843 <chenjj6843@yeastar.com>
2023-09-07 11:46:21 +0800

    修车最短时间和移除数组中指定的值

--- day2/every_day.go
+++ day2/every_day.go
@@ -1,1 +1,26 @@
 package day2
+
+import "math"
+
+// 修车的最少时间 二分查找最小时间
+
+func repairCars(ranks []int, cars int) int64 {
+	l, r := 1, ranks[0]*cars*cars
+	check := func(m int) bool {
+		cnt := 0
+		for _, v := range ranks {
+			cnt += int(math.Sqrt(float64(m / v)))
+		}
+		return cnt >= cars
+	}
+
+	for l < r {
+		m := (l + r) >> 1
+		if check(m) {
+			r = m
+		} else {
+			l = m + 1
+		}
+	}
+	return int64(l)
+}
--- day2/two.go
+++ day2/two.go
@@ -1,1 +1,29 @@
 package day2
+
+//移除数组中指定的值
+
+// 方式一：双指针
+func removeElement(nums []int, val int) int {
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != val {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+	return i
+}
+
+// 方式二:优化
+func removeElement2(nums []int, val int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		if nums[left] == val {
+			nums[left] = nums[right-1]
+			right--
+		} else {
+			left++
+		}
+	}
+	return left
+}
