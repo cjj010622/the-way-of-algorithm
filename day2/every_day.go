3087d000bd9727296e2d7a4589f97745cbcf4296
chenjj6843 <chenjj6843@yeastar.com>
2023-09-08 09:41:02 +0800

    具体题目解释

--- day2/every_day.go
+++ day2/every_day.go
@@ -2,7 +2,15 @@ package day2
 
 import "math"
 
-// 修车的最少时间 二分查找最小时间
+/*给你一个整数数组 ranks ，表示一些机械工的 能力值 。ranksi 是第 i 位机械工的能力值。能力值为 r 的机械工可以在 r * n2 分钟内修好 n 辆车。
+
+同时给你一个整数 cars ，表示总共需要修理的汽车数目。
+
+请你返回修理所有汽车 最少 需要多少时间。
+
+注意：所有机械工可以同时修理汽车。
+
+*/
 
 func repairCars(ranks []int, cars int) int64 {
 	l, r := 1, ranks[0]*cars*cars
