f0570e7b39fb13f9909c8d5c8ca0a5d2f75e43f7
cjj010622 <1612103803@qq.com>
2023-09-21 10:55:57 +0800

    子序列和课程表1

---
+++ day16/sixteen.go
@@ -0,0 +1,18 @@
+package day16
+
+/*
+给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+*/
+
+func isSubsequence(s string, t string) bool {
+	i, j := 0, 0
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+	return i == len(s)
+}
