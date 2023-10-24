5688fe1c642cab09f683006fdb0ccd07073e193b
cjj010622 <1612103803@qq.com>
2023-10-24 20:14:49 +0800

    投骰子等于目标数的方法数

---
+++ day48/everyday.go
@@ -0,0 +1,33 @@
+package day48
+
+/*
+这里有 n 个一样的骰子，每个骰子上都有 k 个面，分别标号为 1 到 k 。
+
+给定三个整数 n ,  k 和 target ，返回可能的方式(从总共 kn 种方式中)滚动骰子的数量，使正面朝上的数字之和等于 target 。
+
+答案可能很大，你需要对 109 + 7 取模 。
+*/
+
+func numRollsToTarget(n int, k int, target int) int {
+	const mod int = 1e9 + 7
+	f := make([][]int, n+1)
+	for i := range f {
+		f[i] = make([]int, target+1)
+	}
+	f[0][0] = 1
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= min(target, i*k); j++ {
+			for h := 1; h <= min(j, k); h++ {
+				f[i][j] = (f[i][j] + f[i-1][j-h]) % mod
+			}
+		}
+	}
+	return f[n][target]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
