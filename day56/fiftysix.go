a5583681866b620c933976d8d48739d98f78caea
cjj010622 <1612103803@qq.com>
2023-11-01 19:24:19 +0800

    和最大的连续子数组

---
+++ day56/fiftysix.go
@@ -0,0 +1,20 @@
+package day56
+
+/*
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组 是数组中的一个连续部分。
+*/
+
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] = nums[i-1] + nums[i]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
