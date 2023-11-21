cac7c96c81e4445c97a28225926c94c9a501d193
cjj010622 <1612103803@qq.com>
2023-11-21 21:09:32 +0800

    每日一题

---
+++ day59/fiftynine.go
@@ -0,0 +1,24 @@
+package day59
+
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+*/
+
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return left
+}
