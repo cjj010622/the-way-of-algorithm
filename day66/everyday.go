5211e5b7c5885cea36c726a0adad3252f32604ca
cjj010622 <1612103803@qq.com>
2023-12-12 14:43:36 +0800

    每日一题

---
+++ day66/everyday.go
@@ -0,0 +1,31 @@
+package day66
+
+/*
+给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+
+注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+*/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	res := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			res = append(res, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			res = append(res, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			res = append(res, nums1[p1])
+			p1++
+		} else {
+			res = append(res, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, res)
+}
